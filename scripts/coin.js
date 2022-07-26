c6b30d7eb6bc83db877a99ba3fef75f02b1f7b73
ctYurk15 <levgenetic@gmail.com>
2022-07-26 13:22:35 +0300

    Add collectable bonuses

---
+++ scripts/coin.js
@@ -0,0 +1,9 @@
+class Coin extends Rectangle
+{
+    paralax_force = 1;
+    
+    constructor(x, y, width, height, color)
+    {
+        super(x, y, width, height, color);
+    }
+}
--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,32 @@
 let player = null;
 let platforms = [];
+let coins = [];
+let coins_collected = 0;
 
-function start(engine, platforms_map)
+function start(engine, level_map)
 {
+    //reset values
     player = null;
     platforms = [];
+    coins = [];
+    coins_collected = 0;
 
-    platforms_map.forEach(platform => {
+    //reset ui
+    coins_container.innerHTML = 'Coins: 0';
+
+    //rebuild level
+    level_map.platforms.forEach(platform => {
         platforms.push(new Platform(platform.x, platform.y, platform.width, platform.height, platform.color));
+        engine.addObject(platforms[platforms.length-1]);
     });
 
-    platforms.forEach(platform => {
-        engine.addObject(platform);
+    level_map.coins.forEach(coin => {
+        coins.push(new Coin(coin.x, coin.y, coin.width, coin.height, coin.color));
+        engine.addObject(coins[coins.length-1]);
     });
 
-    player = new Player(120, 90, 50, 100, 'red', 5, 10, 65, [100, 500], engine);
+    //respawn player
+    player = new Player(120, 120, 50, 100, 'red', 5, 10, 65, [100, 500], engine);
     engine.addObject(player);
     
     engine.start();
@@ -22,6 +34,7 @@ function start(engine, platforms_map)
 
 const canvas = document.querySelector('canvas');
 const win_model = document.querySelector('.win-container');
+const coins_container = document.querySelector('#coinsContainer');
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
@@ -32,18 +45,28 @@ const death_interval = 450;
 const engine = new Engine(canvas, 'white');
 
 //creating gamemap
-const platforms_map = [
-    new Platform(0, canvas.height-20, 250, 20, 'green'),
-    new Platform(300, canvas.height-100, 400, 20, 'green'),
-    new Platform(850, canvas.height-100, 175, 20, 'green'),
-    new Platform(1100, canvas.height-200, 250, 20, 'green'),
-    new Platform(1500, canvas.height-300, 230, 20, 'green'),
-    new Platform(1900, canvas.height-375, 175, 20, 'green'),
-    new Platform(2350, canvas.height-50, 200, 20, 'green'),
-    new Platform(2750, canvas.height-100, 400, 20, 'green'),
+const levels_map = [
+    {
+        coins: 
+        [
+            new Coin(475, canvas.height-150, 50, 50, 'yellow'),
+            new Coin(1590, canvas.height-350, 50, 50, 'yellow'),
+            new Coin(1962.5, canvas.height-425, 50, 50, 'yellow'),
+        ],
+        platforms: [
+            new Platform(0, canvas.height-20, 250, 20, 'green'),
+            new Platform(300, canvas.height-100, 400, 20, 'green'),
+            new Platform(850, canvas.height-100, 175, 20, 'green'),
+            new Platform(1100, canvas.height-200, 250, 20, 'green'),
+            new Platform(1500, canvas.height-300, 230, 20, 'green'),
+            new Platform(1900, canvas.height-375, 175, 20, 'green'),
+            new Platform(2350, canvas.height-50, 200, 20, 'green'),
+            new Platform(2750, canvas.height-100, 400, 20, 'green'),
+        ]
+    }
 ];
 
-//adding collision checks
+//adding platforms collision checks
 engine.addFrameAction(function(){
     let player_can_fall = true;
     let new_player_y = player.y;
@@ -65,6 +88,30 @@ engine.addFrameAction(function(){
     if(!player_can_fall) player.stop(new_player_y);
 });
 
+//adding coins collisions check
+engine.addFrameAction(function(){
+    
+    let collected_coin_id = null;
+
+    for(let coin of coins)
+    {
+        if(player.x + player.width >= coin.x 
+            && player.x + player.width <= coin.x + coin.width
+            && player.y + player.height >= coin.y
+            && player.y + player.height <= coin.y + coin.height)
+        {
+            coins_collected++;
+            coins_container.innerHTML = 'Coins: '+coins_collected;
+            collected_coin_id = coin.id;
+            engine.deleteObject(coin.id);
+            break;
+        }
+    }
+
+    if(collected_coin_id != null) coins = coins.filter(function(coin){ return coin.id != collected_coin_id });
+
+});
+
 //check for winning
 engine.addFrameAction(function(){
     if(player.paralax_scroll >= win_distance)
@@ -82,7 +129,7 @@ engine.addFrameAction(function(){
 
         setTimeout(function(){
             engine.clearObjects();
-            start(engine, platforms_map);
+            start(engine, levels_map[0]);
         }, death_interval);
     }
 });
@@ -96,7 +143,7 @@ engine.addButtonReleaseEvent('KeyD', function(){ player.stopMove(); });
 
 engine.addButtonPressEvent('KeyW', function(){  player.jump(); });
 
-start(engine, platforms_map);
+start(engine, levels_map[0]);
 
 //engine.start();
 let animation_frame;
